c2381ddbc37888a12c1fdf4b2459950b9d4088e2
Jerry <jerry_git@163.com>
2016-05-10 06:39:36 +0800

    format change

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -2980,7 +2980,7 @@ set_control_mode()
 		control_mode.flag_control_attitude_enabled = stabilization_required();
 		control_mode.flag_control_altitude_enabled = false;
 		control_mode.flag_control_climb_rate_enabled = false;
-		control_mode.flag_control_position_enabled = false;
+        control_mode.flag_control_position_enabled = false;
 		control_mode.flag_control_velocity_enabled = false;
 		control_mode.flag_control_termination_enabled = false;
 		break;
